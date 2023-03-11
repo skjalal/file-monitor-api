725f755b553fbd71d8523396026c05e3efabcf21
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-03-11 19:11:15 +0530

    added python script

--- src/main/resources/scripts/file-info.py
+++ src/main/resources/scripts/file-info.py
@@ -9,19 +9,19 @@ path = r"/var/local/test.txt"
 # file modification timestamp of a file
 m_time = os.path.getmtime(path)
 # convert timestamp into DateTime object
-dt_m = datetime.datetime.fromtimestamp(m_time)
+dt_m = datetime.fromtimestamp(m_time)
 print('Modified on:', dt_m)
 
 # file creation timestamp in float
 c_time = os.path.getctime(path)
 # convert creation timestamp into DateTime object
-dt_c = datetime.datetime.fromtimestamp(c_time)
+dt_c = datetime.fromtimestamp(c_time)
 print('Created on:', dt_c)
 
 # file last access timestamp in float
 a_time = os.path.getatime(path)
 # convert last access timestamp into DateTime object
-dt_a = datetime.datetime.fromtimestamp(a_time)
+dt_a = datetime.fromtimestamp(a_time)
 print('Accessed on:', dt_a)
 
 print('File size: ', os.path.getsize(path))
