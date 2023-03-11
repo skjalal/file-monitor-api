91dc27aa16b14d30930b54dce47e58720e36aff4
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-03-11 18:46:20 +0530

    added python script

---
+++ src/main/resources/scripts/file-info.py
@@ -0,0 +1,35 @@
+import datetime
+import os
+import pwd
+
+# Path to the file
+path = r"/var/local/test.txt"
+
+# file modification timestamp of a file
+m_time = os.path.getmtime(path)
+# convert timestamp into DateTime object
+dt_m = datetime.datetime.fromtimestamp(m_time)
+print('Modified on:', dt_m)
+
+# file creation timestamp in float
+c_time = os.path.getctime(path)
+# convert creation timestamp into DateTime object
+dt_c = datetime.datetime.fromtimestamp(c_time)
+print('Created on:', dt_c)
+
+# file last access timestamp in float
+a_time = os.path.getatime(path)
+# convert last access timestamp into DateTime object
+dt_a = datetime.datetime.fromtimestamp(a_time)
+print('Accessed on:', dt_a)
+
+print('File size: ', os.path.getsize(path))
+
+file_stats = os.stat(path)
+
+print(file_stats)
+
+userinfo = pwd.getpwuid(file_stats.st.st_uid)
+print(userinfo)
+
+print('Owner: ', pwd.getpwuid(file_stats.st_uid).pw_name)
