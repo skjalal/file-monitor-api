addf7a68709776f96a132eabf1dbc87e9cfd3395
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-03-11 19:17:53 +0530

    added python script

--- src/main/resources/scripts/file-info.py
+++ src/main/resources/scripts/file-info.py
@@ -10,25 +10,25 @@ path = r"/var/local/test.txt"
 m_time = os.path.getmtime(path)
 # convert timestamp into DateTime object
 dt_m = datetime.fromtimestamp(m_time)
-print('Modified on:', dt_m)
+print("Modified on:", dt_m)
 
 # file creation timestamp in float
 c_time = os.path.getctime(path)
 # convert creation timestamp into DateTime object
 dt_c = datetime.fromtimestamp(c_time)
-print('Created on:', dt_c)
+print("Created on:", dt_c)
 
 # file last access timestamp in float
 a_time = os.path.getatime(path)
 # convert last access timestamp into DateTime object
 dt_a = datetime.fromtimestamp(a_time)
-print('Accessed on:', dt_a)
+print("Accessed on:", dt_a)
 
-print('File size: ', os.path.getsize(path))
+print("File size: ", os.path.getsize(path))
 
 file_stats = os.stat(path)
 
-print('Owner: ', pwd.getpwuid(file_stats.st_uid).pw_name)
+print("Owner: ", pwd.getpwuid(file_stats.st_uid).pw_name)
 
 cmd = "sudo ausearch -k test-file -i"
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
