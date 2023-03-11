03c296cc1e1cb62de8db280f9525c90d9f943839
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-03-11 19:25:36 +0530

    added python script

--- src/main/resources/scripts/file-info.py
+++ src/main/resources/scripts/file-info.py
@@ -2,7 +2,9 @@ from datetime import datetime
 import os
 import pwd
 import subprocess
+import json
 
+data = {}
 # Path to the file
 path = r"/var/local/test.txt"
 
@@ -10,25 +12,25 @@ path = r"/var/local/test.txt"
 m_time = os.path.getmtime(path)
 # convert timestamp into DateTime object
 dt_m = datetime.fromtimestamp(m_time)
-print("Modified on:", dt_m)
+data['lastModifiedDate'] = dt_m
 
 # file creation timestamp in float
 c_time = os.path.getctime(path)
 # convert creation timestamp into DateTime object
 dt_c = datetime.fromtimestamp(c_time)
-print("Created on:", dt_c)
+data['createdDate'] = dt_c
 
 # file last access timestamp in float
 a_time = os.path.getatime(path)
 # convert last access timestamp into DateTime object
 dt_a = datetime.fromtimestamp(a_time)
-print "Accessed on:", dt_a
+data['lastAccessedDate'] = dt_a
 
-print("File size: ", os.path.getsize(path))
+data['fileSize'] = os.path.getsize(path)
 
 file_stats = os.stat(path)
 
-print("Owner: ", pwd.getpwuid(file_stats.st_uid).pw_name)
+data['createdBy'] = pwd.getpwuid(file_stats.st_uid).pw_name
 
 cmd = "sudo ausearch -k test-file -i"
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
@@ -36,5 +38,6 @@ p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 p_status = p.wait()
 out1 = output[output.rindex("type=SYSCALL"):]
 result = out1[out1.index(" uid="):out1.index("gid")].replace("uid=", "")
-print result.strip()
-print "Command exit status/return code : ", p_status
+data['lastModifiedBy'] = result.strip()
+print json.dumps(data)
+print ("Command exit status/return code : ", p_status)
