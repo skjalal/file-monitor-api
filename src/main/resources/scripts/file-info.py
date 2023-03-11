55e23c420304bd8d58c5c16f5e97aea06139f0eb
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2023-03-11 19:03:29 +0530

    added python script

--- src/main/resources/scripts/file-info.py
+++ src/main/resources/scripts/file-info.py
@@ -34,6 +34,6 @@ cmd = "sudo ausearch -k test-file -i"
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 (output, err) = p.communicate()
 p_status = p.wait()
-result = output[output.indexOf("uid", output.indexOf("uid") + 1):output.indexOf("gid")].replace("uid=", "")
+result = output[output.index("uid", output.index("uid") + 1):output.index("gid")].replace("uid=", "")
 print result
 print "Command exit status/return code : ", p_status
